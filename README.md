13c1e178656f2f7c5100044c3f258f8abc2c0845
Steve Bentley <steve.bentley@chorus.co>
2016-10-28 08:24:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# lingo
+
+Linear Algerbra library for Go.
+
+Very much experimental and to help me learn (or atleast relearn) Linear Algerbra. This is in no way ready for use.
---
+++ matrix.go
@@ -0,0 +1,14 @@
+package lingo
+
+type row struct {
+	cell []int
+}
+
+type Matrix struct {
+	rows []row
+}
+
+// Print the matrix to stdout
+func (m *Matrix) Print() {
+	// print the matrix
+}
